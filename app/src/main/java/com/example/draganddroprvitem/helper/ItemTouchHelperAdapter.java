1533eac213812fec10405666c1defd41186608a1
Mardiyev Abbos <abbosmardiev1999@gmail.com>
2022-01-14 16:33:13 +0500

    Initial commit

---
+++ app/src/main/java/com/example/draganddroprvitem/helper/ItemTouchHelperAdapter.java
@@ -0,0 +1,9 @@
+package com.example.draganddroprvitem.helper;
+
+public interface ItemTouchHelperAdapter {
+
+    void onItemMove(int fromPosition, int toPosition);
+
+    void onItemDismiss(int position);
+
+}
